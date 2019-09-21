cf6dd09a5c5b02092b8166ace99f1ad13723f228
Andrew Kane <andrew@chartkick.com>
2019-09-20 19:38:53 -0700

    Test decode_gif [skip ci]

--- test/image_test.rb
+++ test/image_test.rb
@@ -1,5 +1,9 @@
 require_relative "test_helper"
 
 class ImageTest < Minitest::Test
-  # todo
+  def test_decode_gif
+    assert_raises(TensorFlow::Error) do
+      Tf::Image.decode_gif("hi")
+    end
+  end
 end
