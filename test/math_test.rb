afad7df3c9c46d21b4219822334e0436fc393ccd
Andrew Kane <andrew@chartkick.com>
2019-09-20 19:25:21 -0700

    Split out math tests

---
+++ test/math_test.rb
@@ -0,0 +1,54 @@
+require_relative "test_helper"
+
+class MathTest < Minitest::Test
+  def test_abs
+    assert_equal 1, Tf.abs(Tf.constant(-1)).value
+    assert_equal 1, Tf::Math.abs(Tf.constant(-1)).value
+  end
+
+  def test_add
+    a = Tf.constant(2)
+    b = Tf.constant(3)
+    assert_equal 5, (a + b).value
+    assert_equal 5, Tf.add(a, b).value
+    assert_equal 5, Tf::Math.add(a, b).value
+  end
+
+  def test_divide
+    a = Tf.constant(3.0)
+    b = Tf.constant(2.0)
+    assert_in_delta 1.5, (a / b).value
+    assert_in_delta 1.5, Tf.divide(a, b).value
+    assert_in_delta 1.5, Tf::Math.divide(a, b).value
+  end
+
+  def test_multiply
+    a = Tf.constant(2)
+    b = Tf.constant(3)
+    assert_equal 6, (a * b).value
+    assert_equal 6, Tf.multiply(a, b).value
+    assert_equal 6, Tf::Math.multiply(a, b).value
+
+    a = Tf.constant([[1, 2], [3, 4]])
+    b = Tf.add(a, 1)
+    assert_equal [[2, 6], [12, 20]], (a * b).value
+  end
+
+  def test_sin
+    assert_equal [0, 1], Tf.sin([0.0, 0.5 * Math::PI]).value
+    assert_equal [0, 1], Tf::Math.sin([0.0, 0.5 * Math::PI]).value
+  end
+
+  def test_sqrt
+    assert_equal 3.0, Tf.sqrt(Tf.constant(9.0)).value
+    assert_equal 3.0, Tf::Math.sqrt(Tf.constant(9.0)).value
+  end
+
+  def test_subtract
+    a = Tf.constant(2)
+    b = Tf.constant(3)
+    assert_equal -1, (a - b).value
+    assert_equal -1, Tf.subtract(a, b).value
+    assert_equal -1, Tf::Math.subtract(a, b).value
+  end
+end
--- test/operations_test.rb
+++ test/operations_test.rb
@@ -1,25 +1,6 @@
 require_relative "test_helper"
 
 class OperationsTest < Minitest::Test
-  # keep in alphabetical order
-
-  def test_abs
-    assert_equal 1, Tf.abs(Tf.constant(-1)).value
-  end
-
-  def test_add
-    a = Tf.constant(2)
-    b = Tf.constant(3)
-    assert_equal 5, (a + b).value
-  end
-
-  def test_divide
-    a = Tf.constant(2)
-    b = Tf.constant(3)
-    assert_in_delta 0, (a / b).value
-    assert_in_delta 0, Tf.divide(a, b).value
-  end
-
   def test_equal
     x = Tf.constant([2, 4])
     y = Tf.constant(2)
@@ -65,17 +46,6 @@ class OperationsTest < Minitest::Test
     assert_equal [[4.0]], Tf.matmul(x, x).value
   end
 
-  def test_multiply
-    a = Tf.constant(2)
-    b = Tf.constant(3)
-    assert_equal 6, (a * b).value
-    assert_equal 6, Tf.multiply(a, b).value
-
-    a = Tf.constant([[1, 2], [3, 4]])
-    b = Tf.add(a, 1)
-    assert_equal [[2, 6], [12, 20]], (a * b).value
-  end
-
   def test_ones
     assert_equal [[1, 1, 1], [1, 1, 1]], Tf.ones([2, 3]).value
   end
@@ -83,21 +53,6 @@ class OperationsTest < Minitest::Test
   def test_range
     assert_equal [0, 1, 2], Tf.range(3).value
     assert_equal [3, 6, 9, 12, 15], Tf.range(3, 18, 3).value
-  end
-
-  def test_sin
-    assert_equal [0, 1], Tf.sin([0.0, 0.5 * Math::PI]).value
-  end
-
-  def test_sqrt
-    assert_equal 3.0, Tf.sqrt(Tf.constant(9.0)).value
-  end
-
-  def test_subtract
-    a = Tf.constant(2)
-    b = Tf.constant(3)
-    assert_equal -1, (a - b).value
-    assert_equal -1, Tf.subtract(a, b).value
   end
 
   def test_timestamp
