79184dd8b1256093f562a41a0a58625741e54233
Andrew Kane <andrew@chartkick.com>
2019-09-21 23:12:34 -0700

    Prep for other datasets [skip ci]

--- test/datasets_test.rb
+++ test/datasets_test.rb
@@ -11,6 +11,26 @@ class DatasetsTest < Minitest::Test
     assert_equal [102], y_test.shape
   end
 
+  # def test_fashion_mnist
+  #   fashion_mnist = Tf::Keras::Datasets::FashionMNIST
+  #   (x_train, y_train), (x_test, y_test) = mnist.load_data
+
+  #   assert_equal [60000, 28, 28], x_train.shape
+  #   assert_equal [60000], y_train.shape
+  #   assert_equal [10000, 28, 28], x_test.shape
+  #   assert_equal [10000], y_test.shape
+  # end
+
+  # def test_imdb
+  #   imdb = Tf::Keras::Datasets::IMDB
+  #   (x_train, y_train), (x_test, y_test) = imdb.load_data
+
+  #   assert_equal [25000], x_train.shape
+  #   assert_equal [25000], y_train.shape
+  #   assert_equal [25000], x_test.shape
+  #   assert_equal [25000], y_test.shape
+  # end
+
   def test_mnist
     mnist = Tf::Keras::Datasets::MNIST
     (x_train, y_train), (x_test, y_test) = mnist.load_data
@@ -20,4 +40,14 @@ class DatasetsTest < Minitest::Test
     assert_equal [10000, 28, 28], x_test.shape
     assert_equal [10000], y_test.shape
   end
+
+  # def test_reuters
+  #   reuters = Tf::Keras::Datasets::Reuters
+  #   (x_train, y_train), (x_test, y_test) = reuters.load_data
+
+  #   assert_equal [8982], x_train.shape
+  #   assert_equal [8982], y_train.shape
+  #   assert_equal [2246], x_test.shape
+  #   assert_equal [2246], y_test.shape
+  # end
 end
--- test/support/datasets.py
+++ test/support/datasets.py
@@ -5,7 +5,32 @@ boston_housing = tf.keras.datasets.boston_housing
 (x_train, y_train),(x_test, y_test) = boston_housing.load_data()
 print(x_train.shape, y_train.shape, x_test.shape, y_test.shape)
 
+# print("cifar10")
+# cifar10 = tf.keras.datasets.cifar10
+# (x_train, y_train),(x_test, y_test) = cifar10.load_data()
+# print(x_train.shape, y_train.shape, x_test.shape, y_test.shape)
+
+# print("cifar100")
+# cifar100 = tf.keras.datasets.cifar100
+# (x_train, y_train),(x_test, y_test) = cifar100.load_data()
+# print(x_train.shape, y_train.shape, x_test.shape, y_test.shape)
+
+print("fashion_mnist")
+fashion_mnist = tf.keras.datasets.fashion_mnist
+(x_train, y_train),(x_test, y_test) = fashion_mnist.load_data()
+print(x_train.shape, y_train.shape, x_test.shape, y_test.shape)
+
+print("imdb")
+imdb = tf.keras.datasets.imdb
+(x_train, y_train),(x_test, y_test) = imdb.load_data()
+print(x_train.shape, y_train.shape, x_test.shape, y_test.shape)
+
 print("mnist")
 mnist = tf.keras.datasets.mnist
 (x_train, y_train),(x_test, y_test) = mnist.load_data()
+print(x_train.shape, y_train.shape, x_test.shape, y_test.shape)
+
+print("reuters")
+reuters = tf.keras.datasets.reuters
+(x_train, y_train),(x_test, y_test) = reuters.load_data()
 print(x_train.shape, y_train.shape, x_test.shape, y_test.shape)
