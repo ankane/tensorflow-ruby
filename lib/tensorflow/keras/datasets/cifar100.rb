b2fed508811f67cb5752ce13485b4af0493a05e6
Andrew Kane <andrew@chartkick.com>
2019-09-22 00:20:53 -0700

    Can't read pickle [skip ci]

--- lib/tensorflow/keras/datasets/cifar100.rb
+++ lib/tensorflow/keras/datasets/cifar100.rb
@@ -2,6 +2,7 @@ module TensorFlow
   module Keras
     module Datasets
       module CIFAR100
+        # unfortunately, npy can't read pickle and numo can't store objects
         # def self.load_data
         # end
       end
