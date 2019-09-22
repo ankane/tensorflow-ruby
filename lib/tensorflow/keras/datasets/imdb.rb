4b06ebbaea2bbeb1430e7601202b119cf98234f9
Andrew Kane <andrew@chartkick.com>
2019-09-21 23:53:20 -0700

    Unable to load IMDB format [skip ci]

--- lib/tensorflow/keras/datasets/imdb.rb
+++ lib/tensorflow/keras/datasets/imdb.rb
@@ -2,7 +2,18 @@ module TensorFlow
   module Keras
     module Datasets
       module IMDB
-        # def self.load_data
+        # unfortunately, npy can't read pickle and numo can't store objects
+        # def self.load_data(path: "imdb.npz", seed: 113)
+        #   data = Utils.load_dataset(
+        #     path,
+        #     "https://storage.googleapis.com/tensorflow/tf-keras-datasets/imdb.npz",
+        #     "69664113be75683a8fe16e3ed0ab59fda8886cb3cd7ada244f7d9544e4676b9f"
+        #   )
+
+        #   x_train = data["x_train"]
+        #   labels_train = data["y_train"]
+        #   x_test = data["x_test"]
+        #   labels_test = data["y_test"]
         # end
 
         def self.get_word_index(path: "imdb_word_index.json")
