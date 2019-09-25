6012931c384803b566a3bc460d5964eb3addba46
Andrew Kane <andrew@chartkick.com>
2019-09-25 00:40:29 -0700

    Cleaner code

--- lib/tensorflow/keras/model.rb
+++ lib/tensorflow/keras/model.rb
@@ -2,7 +2,10 @@ module TensorFlow
   module Keras
     class Model
       def trainable_variables
-        instance_variables.map { |v| instance_variable_get(v) }.select { |layer| layer.respond_to?(:trainable_variables) }.flat_map { |layer| layer.trainable_variables }
+        instance_variables
+          .map { |v| instance_variable_get(v) }
+          .select { |layer| layer.respond_to?(:trainable_variables) }
+          .flat_map { |layer| layer.trainable_variables }
       end
     end
   end
