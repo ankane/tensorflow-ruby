a1c25ae4f4a7b240ba7b04da53bd6eef0b21463d
Andrew Kane <andrew@chartkick.com>
2019-09-25 03:47:31 -0700

    WIP

--- lib/tensorflow/gradient_tape.rb
+++ lib/tensorflow/gradient_tape.rb
@@ -4,10 +4,18 @@ module TensorFlow
       context = Utils.default_context
       context.start_step
 
+      @tensor_tape = {}
+      @tensor_usage = {}
+
       yield(self) if block_given?
     end
 
     def watch(tensor)
+      i = 1 # tensor_id
+      unless @tensor_tape[i]
+        @tensor_tape[i] ||= tensor
+        @tensor_usage[i] = 1
+      end
     end
 
     def gradient(target, sources)
