569497b2fe4c88a0251a896a884ea9695096c79b
Andrew Kane <andrew@chartkick.com>
2019-09-25 00:24:21 -0700

    Added SparseCategoricalAccuracy

--- lib/tensorflow/keras/metrics/sparse_categorical_accuracy.rb
+++ lib/tensorflow/keras/metrics/sparse_categorical_accuracy.rb
@@ -1,14 +1,22 @@
 module TensorFlow
   module Keras
     module Metrics
-      class SparseCategoricalAccuracy
-        def initialize(name: nil)
-        end
-
+      class SparseCategoricalAccuracy < Mean
         def call(y_true, y_pred)
+          update_state(y_true, y_pred)
         end
 
-        def reset_states
+        def update_state(y_true, y_pred)
+          y_true = TensorFlow.convert_to_tensor(y_true)
+          y_pred = TensorFlow.convert_to_tensor(y_pred)
+
+          y_pred = RawOps.arg_max(input: y_pred, dimension: -1)
+
+          if y_pred.dtype != y_true.dtype
+            y_pred = TensorFlow.cast(y_pred, y_true.dtype)
+          end
+
+          super(Math.equal(y_true, y_pred))
         end
       end
     end
