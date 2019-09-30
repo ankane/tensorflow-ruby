bc6c0692aadd9780ba3150dc54f1dfba006150b9
Andrew Kane <andrew@chartkick.com>
2019-09-30 13:49:33 -0700

    No dash

--- lib/tensorflow/keras/models/sequential.rb
+++ lib/tensorflow/keras/models/sequential.rb
@@ -25,7 +25,7 @@ module TensorFlow
 
             samples_width = sample_size.to_s.size
             title = "%#{samples_width}d/#{sample_size}" % [1]
-            progressbar = ProgressBar.create(total: sample_size, length: 49 + title.size, format: "%t [%B] - %e", remainder_mark: ".")
+            progressbar = ProgressBar.create(total: sample_size, length: 47 + title.size, format: "%t [%B] %e", remainder_mark: ".")
             sample_size.times do |i|
               progressbar.title = "%#{samples_width}d/#{sample_size}" % [i + 1]
               progressbar.increment
